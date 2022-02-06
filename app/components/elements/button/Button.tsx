452ec184410ff8ecb3db39559a88975abc433e19
Sam Poate <sampoate@outlook.com>
2022-02-06 10:22:48 +0000

    Add error message to button

--- app/components/elements/button/Button.tsx
+++ app/components/elements/button/Button.tsx
@@ -1,17 +1,25 @@
 import { ButtonHTMLAttributes } from 'react';
+import { GoAlert } from 'react-icons/go';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     buttonType?: 'primary' | 'secondary' | 'tertiary' | 'outline';
+    error?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ buttonType, className, children, ...props }) => (
+export const Button: React.FC<ButtonProps> = ({ buttonType, className, error, children, ...props }) => (
     <button
         type='button'
         className={[styles.button, styles[buttonType || 'primary'], className || ''].join(' ').trim()}
         {...props}
     >
         {children}
+        {error && (
+            <div className={styles.errorMessage}>
+                <GoAlert />
+                <p>{error}</p>
+            </div>
+        )}
     </button>
 );
 
